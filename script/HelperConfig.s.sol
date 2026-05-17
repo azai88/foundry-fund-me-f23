857aa3e38c7e3277a342a9dccdbe4078b59754b6
Azai <azaigroup5784@gmail.com>
2026-05-17 15:34:50 -0500

    add helper config for network deployment

---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {Script} from "forge-std/Script.sol";
+
+contract HelperConfig is Script {
+    NetworkConfig public activeNetworkConfig;
+
+    struct NetworkConfig {
+        address priceFeed;
+    }
+
+    constructor() {
+        if (block.chainid == 11155111) {
+            activeNetworkConfig = getSepoliaEthConfig();
+        } else {
+            activeNetworkConfig = getAnvilEthConfig();
+        }
+    }
+
+    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
+        return
+            NetworkConfig({
+                priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+            });
+    }
+
+    function getAnvilEthConfig() public pure returns (NetworkConfig memory) {
+        return NetworkConfig({priceFeed: address(1)});
+    }
+}
